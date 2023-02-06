d40a719c2ea0a7c7249447d5ade20798c28bf3a8
Jacob Gyoerkoe <jacobgyo@ad.unc.edu>
2023-02-06 13:54:55 -0500

    Finised ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""EX02 - One shot wordle"""
+"""EX02 - One shot wordle."""
 
 __author__ = "730551323"
 
@@ -12,24 +12,24 @@ guess: str = input(f"What is your {len(SECRET_WORD)}-letter guess? ")
 while len(guess) != len(SECRET_WORD):
     guess: str = input(f"That was not {len(SECRET_WORD)} letters! Try again: ")
 while guess_index < len(SECRET_WORD):
-        if SECRET_WORD[guess_index] == guess[guess_index]:
+    if SECRET_WORD[guess_index] == guess[guess_index]:
             INDEX = INDEX + GREEN_BOX  
-        else:
-            guessed_character: bool = False
-            secret_index: int = 0
-            while guessed_character == False and secret_index < len(SECRET_WORD):
-                if SECRET_WORD[secret_index] == guess[guess_index]:
-                      guessed_character = True
-                else:
-                     secret_index = secret_index + 1
-            if guessed_character == True:
-                 INDEX = INDEX + YELLOW_BOX
+    else:
+        guessed_character: bool = False
+        secret_index: int = 0
+        while guessed_character != True and secret_index < len(SECRET_WORD):
+            if SECRET_WORD[secret_index] == guess[guess_index]:
+                guessed_character = True
             else:
-                INDEX = INDEX + WHITE_BOX
+                secret_index = secret_index + 1
+            if guessed_character != False:
+                 INDEX = INDEX + YELLOW_BOX
+        else:
+            INDEX = INDEX + WHITE_BOX
         guess_index = guess_index + 1
 print(INDEX)
 
 if guess == SECRET_WORD:
     print("Woo! You got it!")
-else: #guess != SECRET_WORD
+else:  #guess != SECRET_WORD
     print("Not quite. Play again soon!")
